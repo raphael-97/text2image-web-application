91dcefb7dc35216c6bbdab508190e74b27dccbc0
Raph <63296115+Raphael-97@users.noreply.github.com>
2024-01-29 11:32:59 +0100

    Add navigationbar to layout

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,9 +1,9 @@
-import Image from "next/image";
+import { Button } from "@nextui-org/react";
 
 export default function Home() {
   return (
-    <main>
-      main
-    </main>
+    <div>
+      <Button>Click me</Button>
+    </div>
   );
 }
