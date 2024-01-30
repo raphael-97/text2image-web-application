1270ffc2e76439c8e6ca5e7bff7e8a33a048948d
Raph <63296115+Raphael-97@users.noreply.github.com>
2024-01-30 11:24:16 +0100

    Add empty Spring Boot backend application with configured postgresql properties

---
+++ backend/src/main/java/com/noCompany/BackendStableDiffusionWebApp/BackendStableDiffusionWebAppApplication.java
@@ -0,0 +1,13 @@
+package com.noCompany.BackendStableDiffusionWebApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendStableDiffusionWebAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendStableDiffusionWebAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/noCompany/BackendStableDiffusionWebApp/BackendStableDiffusionWebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.noCompany.BackendStableDiffusionWebApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendStableDiffusionWebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
