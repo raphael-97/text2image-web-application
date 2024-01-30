f2d1eac924aca16e7c1d39e080f41fde66d7bcac
Raph <63296115+Raphael-97@users.noreply.github.com>
2024-01-30 05:04:32 +0100

    Fix ThemeSwitcher component
    ThemeSwitcher shows wrong button after page refresh because of hydration mismatch.
    
    https://github.com/pacocoursey/next-themes?tab=readme-ov-file#css

--- frontend/src/components/ThemeSwitcher.tsx
+++ frontend/src/components/ThemeSwitcher.tsx
@@ -2,22 +2,34 @@
 
 import { Button } from "@nextui-org/react";
 import {useTheme} from "next-themes";
-import { useState } from "react";
+import { useEffect, useLayoutEffect, useState } from "react";
 import { MdOutlineLightMode } from "react-icons/md";
 import { MdOutlineDarkMode } from "react-icons/md";
 
 export function ThemeSwitcher() {
-  const [mounted, setMounted] = useState(true)
+  
   const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
 
 
   const changeTheme = () => {
-    mounted ? setTheme("light") : setTheme("dark")
-    setMounted(!mounted)
+    theme === "dark" ? setTheme("light") : setTheme("dark") 
+  }
+
+
+  useEffect(() => {
+    setMounted(true)
+  }, [])
+
+  if(!mounted) {
+    return <>
+      <Button className='dark:hidden bg-inherit' variant='solid' size='lg' disableRipple disableAnimation isIconOnly><MdOutlineDarkMode /></Button>
+      <Button className='hidden dark:flex bg-inherit ml-2' variant='solid' size='lg' disableRipple disableAnimation isIconOnly><MdOutlineLightMode /></Button>
+    </>
   }
 
   return (
-    mounted ? (
+    theme === "dark" ? (
         <Button className='bg-inherit' variant='solid' size='lg' disableRipple  disableAnimation isIconOnly onClick={changeTheme}><MdOutlineLightMode /></Button>
     ) : (
         <Button className='bg-inherit' variant='solid' size='lg' disableRipple disableAnimation isIconOnly onClick={changeTheme}><MdOutlineDarkMode /></Button>
