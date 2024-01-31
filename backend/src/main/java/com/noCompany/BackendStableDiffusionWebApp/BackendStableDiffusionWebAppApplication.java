f358b8a3d4d1510f41653023be5febffe433b071
Raph <63296115+Raphael-97@users.noreply.github.com>
2024-01-31 12:46:28 +0100

    Add one user in database

--- backend/src/main/java/com/noCompany/BackendStableDiffusionWebApp/BackendStableDiffusionWebAppApplication.java
+++ backend/src/main/java/com/noCompany/BackendStableDiffusionWebApp/BackendStableDiffusionWebAppApplication.java
@@ -1,13 +1,44 @@
 package com.noCompany.BackendStableDiffusionWebApp;
 
+import com.noCompany.BackendStableDiffusionWebApp.domain.User;
+import com.noCompany.BackendStableDiffusionWebApp.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.Set;
 
 @SpringBootApplication
-public class BackendStableDiffusionWebAppApplication {
+public class BackendStableDiffusionWebAppApplication implements CommandLineRunner {
+
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Autowired
+	public BackendStableDiffusionWebAppApplication(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+		this.userRepository = userRepository;
+		this.passwordEncoder = passwordEncoder;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(BackendStableDiffusionWebAppApplication.class, args);
 	}
 
+	// Just for testing :)
+	@Override
+	public void run(String... args) throws Exception {
+
+		User user = User.builder()
+				.username("testuser")
+				.email("testuser@gmail.com")
+				.password(passwordEncoder.encode(passwordEncoder.encode("password")))
+				.credits(100L)
+				.roles(Set.of("USER", "ADMIN"))
+				.build();
+
+		if (!userRepository.findByUsername(user.getUsername()).isPresent())
+			userRepository.save(user);
+	}
 }
